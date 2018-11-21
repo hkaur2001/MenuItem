55ba21d8d2fe6a9c138c9834e71cb0a7150a5fe8
BT_1N3_28@bktech.local <BT_1N3_28@bktech.local>
2018-11-21 14:47:12 -0500

    Initial commit

---
+++ src/Trio.java
@@ -0,0 +1,18 @@
+public class Trio implements MenuItem{
+    private Sandwich sandwich;
+    private Salad salad;
+    private Drink drink;
+    private String name;
+    private double price;
+}
+
+public Trio(sandwich, salad, drink){
+    sandwichprice = this.getsandwichprice;
+    saladprice = this.getsaladprice;
+    drinkprice = this.getdrinkprice;
+    sandwichname = this.getsandwichname;
+    saladname = this.getsaladbname;
+    drinkname = this.getdrinkname;
+}
+
+p1 = price
